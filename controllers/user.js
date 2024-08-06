ab21cdee99f1a7c7c3ba2b72bfacbf0775fd8aa2
sibeni-li <pro@sibeni.li>
2024-08-06 22:52:56 +0200

    Verification of email and password added

--- controllers/user.js
+++ controllers/user.js
@@ -25,30 +25,35 @@ exports.signup = (req, res, next) => {
 
 // User login
 exports.login = (req, res, next) => {
-    User.findOne({email: req.body.email})
-        .then(user => {
-            if (!user) {
-                res.status(401).json({message: 'Paire identifiant/mot de passe incorrecte !'});
-            } else {
-                // Compare provided password with stored hash
-                bcrypt.compare(req.body.password, user.password)
-                    .then(valid => {
-                        if (!valid) {
-                            res.status(401).json({message: 'Paire identifiant/mot de passe incorrecte !'});
-                        } else {
-                            // Generate JWT token
-                            res.status(200).json({
-                                userId: user._id,
-                                token: jwt.sign(
-                                    { userId: user._id },
-                                    'RANDOM_TOKEN_SECRET',
-                                    { expiresIn: '24h' }
-                                )
-                            });
-                        };
-                    })
-                    .catch(error =>res.status(500).json({error}));
-            };
-        })
-        .catch(error => res.status(500).json({error}));
+    if (req.body.email && req.body.password) {
+        User.findOne({email: req.body.email})
+            .then(user => {
+                if (!user) {
+                    res.status(401).json({message: 'Paire identifiant/mot de passe incorrecte !'});
+                } else {
+                    // Compare provided password with stored hash
+                    bcrypt.compare(req.body.password, user.password)
+                        .then(valid => {
+                            if (!valid) {
+                                res.status(401).json({message: 'Paire identifiant/mot de passe incorrecte !'});
+                            } else {
+                                // Generate JWT token
+                                res.status(200).json({
+                                    userId: user._id,
+                                    token: jwt.sign(
+                                        { userId: user._id },
+                                        'RANDOM_TOKEN_SECRET',
+                                        { expiresIn: '24h' }
+                                    )
+                                });
+                            };
+                        })
+                        .catch(error =>res.status(500).json({error}));
+                };
+            })
+            .catch(error => res.status(500).json({error}));
+    } else {
+        res.status(400).json({message: 'Champs manquants'});
+    };
+
 };
