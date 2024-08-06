502f430cd14688884c52bef6a69faa5f5acb3cf2
sibeni-li <pro@sibeni.li>
2024-08-06 21:41:25 +0200

    and  added to routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 // Imports
 const express = require('express');
 const userCtrl = require('../controllers/user');
+const limiter = require('../middleware/express-rate-limit-config');
+const validatePassword = require('../middleware/password-validator-config');
 const router = express.Router();
 
 // Define route for user authentication
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', validatePassword, userCtrl.signup); 
+router.post('/login', limiter, userCtrl.login);
 
-module.exports = router;
+module.exports = router; 
