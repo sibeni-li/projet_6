2e809483076a5268fcea7301aaa619de481c11f1
sibeni-li <pro@sibeni.li>
2024-08-11 22:25:00 +0200

    random token secret modified

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
         // Extract the token from the Authorization header
         const token = req.headers.authorization.split(' ')[1];
         // Verify the token
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
         // Extract the userId from the decoded token
         const userId = decodedToken.userId;
         // Add the auth information to the request object
